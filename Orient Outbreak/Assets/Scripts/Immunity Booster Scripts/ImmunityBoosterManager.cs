26df8bc10a45a74a629336ac20ff78ab4ef8dc67
unigalacorn <vilelafranz08@gmail.com>
2021-10-26 02:35:37 +0800

    Added Gab Song

--- Orient Outbreak/Assets/Scripts/Immunity Booster Scripts/ImmunityBoosterManager.cs
+++ Orient Outbreak/Assets/Scripts/Immunity Booster Scripts/ImmunityBoosterManager.cs
@@ -52,7 +52,7 @@ public class ImmunityBoosterManager : MonoBehaviour
     #region Coroutines
     IEnumerator StartSoundtrack()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(1f);
         soundtrack.Play();
     }
     #endregion
